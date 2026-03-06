b6d7a5169a16b5aab8b15ad98517721590903f1e
alison <adevpr11@gmail.com>
2026-03-06 20:05:08 -0300

    feat: adiciona discoveryserverapp e docker-compose base

---
+++ discoveryserverapp/src/test/java/com/nca/discoveryserverapp/DiscoveryserverappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nca.discoveryserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
